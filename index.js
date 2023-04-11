fc8afb345ea2fbe5fad1de619be48a979bd46e44
James Collins <jamesdcollins0723@gmail.com>
2023-04-11 10:19:30 +0900

    change

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ const io = require('socket.io')(socketServer, {
   }
 });
 
+let imageNames = require('./imagenames.json').imageNames;
+
 const readConfig = function() {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, 'config.json'), 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
+    fs.readFile(path.join(__dirname, '/config.json'), 'utf8', (err, data) => {
+      if (err) reject(err);
       else resolve(JSON.parse(data));
     });
   })
@@ -52,25 +52,13 @@ const readImage = function(name) {
   
 }
 
-let configData, imageData;
-
-readConfig()
-  .then(data => {
-    configData = data;
-    readImage(data.IMAGE_NAME)
-      .then(_data => {
-        imageData = _data;
-      })
-      .catch(err => imageData = 0)
-  })
+let configData = null, imageData = null;
 
 io.on('connection', client => {
   client.emit('message', {
-    IMAGE_NAME: config.IMAGE_NAME,
-    IMAGE_X1: config.IMAGE_X1,
-    IMAGE_Y1: config.IMAGE_Y1,
-    IMAGE_WIDTH: (config.IMAGE_X2 - config.IMAGE_X1),
-    IMAGE_HEIGHT: (config.IAMGE_Y2-config.IMAGE_Y1),
+    IMAGE_NAMES: imageNames,
+    IMAGE_WIDTH: config.IMAGE_WIDTH,
+    IMAGE_HEIGHT: config.IAMGE_HEIGHT,
     RECT_X1: config.RECT_X1,
     RECT_Y1: config.RECT_Y1,
     RECT_WIDTH: (config.RECT_X2-config.RECT_X1),
@@ -78,20 +66,34 @@ io.on('connection', client => {
   });
 });
 
+function randomString(length) {
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
 setInterval(() => {
   readConfig()
     .then(data => {
       readImage(data.IMAGE_NAME)
         .then(_data => {
+          if (JSON.stringify(_data) !== JSON.stringify(imageData)) {
+            const newImage = randomString(12);
+            fs.writeFile(path.join(__dirname, '/public/' + newImage), _data, (err, res) => {              
+            });
+            imageNames.unshift(newImage);
+            if (imageNames.length > 3) imageNames.pop();
+            fs.writeFile(path.join(__dirname, '/imagenames.json'), JSON.stringify({imageNames}), (err, res) => {
+            });
+          }
           if (JSON.stringify(data) !== JSON.stringify(configData) || JSON.stringify(_data) !== JSON.stringify(imageData)) {
             configData = data;
             imageData = _data;
             io.emit('message', {
-              IMAGE_NAME: configData.IMAGE_NAME,
-              IMAGE_X1: configData.IMAGE_X1,
-              IMAGE_Y1: configData.IMAGE_Y1,
-              IMAGE_WIDTH: (configData.IMAGE_X2 - configData.IMAGE_X1),
-              IMAGE_HEIGHT: (configData.IAMGE_Y2-configData.IMAGE_Y1),
+              IMAGE_NAMES: imageNames,
+              IMAGE_WIDTH: config.IMAGE_WIDTH,
+              IMAGE_HEIGHT: config.IAMGE_HEIGHT,
               RECT_X1: configData.RECT_X1,
               RECT_Y1: configData.RECT_Y1,
               RECT_WIDTH: (configData.RECT_X2-configData.RECT_X1),
@@ -101,13 +103,11 @@ setInterval(() => {
         })
         .catch(() => {
           configData = data;
-          imageData = 0;
+          imageData = null;
           io.emit('message', {
-            IMAGE_NAME: configData.IMAGE_NAME,
-            IMAGE_X1: configData.IMAGE_X1,
-            IMAGE_Y1: configData.IMAGE_Y1,
-            IMAGE_WIDTH: (configData.IMAGE_X2 - configData.IMAGE_X1),
-            IMAGE_HEIGHT: (configData.IAMGE_Y2-configData.IMAGE_Y1),
+            IMAGE_NAMES: imageNames,
+            IMAGE_WIDTH: config.IMAGE_WIDTH,
+            IMAGE_HEIGHT: config.IAMGE_HEIGHT,
             RECT_X1: configData.RECT_X1,
             RECT_Y1: configData.RECT_Y1,
             RECT_WIDTH: (configData.RECT_X2-configData.RECT_X1),
